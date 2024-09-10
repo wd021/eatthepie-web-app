1bfa5f40272989d7fd08a8d2deb07806a4312020
wd021 <dannywitters@gmail.com>
2024-09-10 15:17:05 +0100

    minor spacing changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,18 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center gap-y-4 text-center px-6">
+    <div className="h-screen flex flex-col items-center justify-center text-center px-6">
       <Image
         src="/logo.png"
         alt="Eat The Pie Lottery"
         width={300}
         height={300}
       />
-      <div className="text-2xl">
+      <div className="text-2xl mt-12">
         The world&apos;s first fully autonomous and trustless lottery built on
         Ethereum.
       </div>
-      <div className="text-2xl">Coming soon...</div>
+      <div className="text-2xl mt-4">Coming soon...</div>
     </div>
   );
 }
