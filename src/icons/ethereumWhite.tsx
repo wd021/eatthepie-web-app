11f24e55dccf5362a5200eaddf045cd7e571da26
wd021 <dannywitters@gmail.com>
2024-12-13 14:22:41 +0000

    update overflow issues

--- src/icons/ethereumWhite.tsx
+++ src/icons/ethereumWhite.tsx
@@ -2,13 +2,7 @@ import React, { type SVGProps } from 'react'
 
 export default function EthereumWhite(props: SVGProps<SVGSVGElement>) {
   return (
-    <svg
-      width='1535'
-      height='2500'
-      viewBox='0 0 256 417'
-      preserveAspectRatio='xMidYMid'
-      {...props}
-    >
+    <svg width='30' height='49' viewBox='0 0 256 417' preserveAspectRatio='xMidYMid' {...props}>
       <path fill='#fff' d='M127.961 0l-2.795 9.5v275.668l2.795 2.79 127.962-75.638z' />
       <path fill='#fff' d='M127.962 0L0 212.32l127.962 75.639V154.158z' />
       <path fill='#fff' d='M127.961 312.187l-1.575 1.92v98.199l1.575 4.6L256 236.587z' />
